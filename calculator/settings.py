bc946062c08ebba9cbd7a392bdb2b092f5e4f93a
sanghoonnam <yoodotan@snu.ac.kr>
2022-09-14 12:28:06 +0900

    set debug to true

--- calculator/settings.py
+++ calculator/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!4_cgn=o)+^3fy)b1zw)s%2+aj9v0_kq6h+q3!8b*2pm&d0+j9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['letscalculate.co.kr', "43.200.51.78","www.letscalculate.co.kr","*"]
-
+ALLOWED_HOSTS = ["*"]
+# 'letscalculate.co.kr', "43.200.51.78","www.letscalculate.co.kr"
 
 # Application definition
 
