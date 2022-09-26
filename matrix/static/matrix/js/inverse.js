4ea6afff6183bee9eeb9f2fe201d68b9af9c3487
sanghoonnam <yoodotan@snu.ac.kr>
2022-09-26 22:56:53 +0900

    inverse logic more compact

--- matrix/static/matrix/js/inverse.js
+++ matrix/static/matrix/js/inverse.js
@@ -20,19 +20,39 @@ const fraction = (num) => {
 }
 
 const fracSum = (a,b) => {
-    a[0] = parseInt(a[0])
-    a[1] = parseInt(a[1])
-    b[0] = parseInt(b[0])
-    b[1] = parseInt(b[1])
-    return [parseInt(a[0]*b[1]+a[1]*b[0]),parseInt(a[1]*b[1])]
+    a1 = parseInt(a[0])
+    a2 = parseInt(a[1])
+    b1 = parseInt(b[0])
+    b2 = parseInt(b[1])
+    let gSum = gcd(a1*b2+a2*b1, a2*b2);
+    return [(a1*b2+a2*b1)/gSum, a2*b2/gSum]
 }
 
 const fracMul = (a,b) =>{
-    a[0] = parseInt(a[0])
-    a[1] = parseInt(a[1])
-    b[0] = parseInt(b[0])
-    b[1] = parseInt(b[1])
-    return [parseInt(a[0]*b[0]),parseInt(a[1]*b[1])]
+    a0 = parseInt(a[0])
+    a1 = parseInt(a[1])
+    b0 = parseInt(b[0])
+    b1 = parseInt(b[1])
+    let gMul = gcd(a0*b0,a1*b1)
+    return [a0*b0/gMul,a1*b1/gMul]
+}
+
+const fracSub = (a,b) => {
+    a0 = parseInt(a[0])
+    a1 = parseInt(a[1])
+    b0 = parseInt(b[0])
+    b1 = parseInt(b[1])
+    let gSub = gcd(a0*b1-a1*b0,a1*b1)
+    return [(a0*b1-a1*b0)/gSub,a1*b1/gSub]
+}
+
+const fracDiv = (a,b) => {
+    a0 = parseInt(a[0])
+    a1 = parseInt(a[1])
+    b0 = parseInt(b[0])
+    b1 = parseInt(b[1])
+    let gDiv = gcd(a0*b1,a1*b0)
+    return [a0*b1/gDiv, a1*b0/gDiv]
 }
 
 const gcd = (a,b) => {
@@ -153,6 +173,174 @@ const detCalc = (arr) => {
     }
 }
 
+
+const inverse = () => {
+    let val = document.getElementById("inputNumber").value;
+    val = parseInt(val);
+    let jaxString = document.getElementById('inverse').innerHTML+"$\\begin{pmatrix}";
+    let jaxMatrix = new Array();
+    const slash = "{ \\\\ }";
+
+    let numMatrix = create2DArray(val,val);
+    for(let i=0;i<val;i++){
+        for(let j=0;j<val;j++){
+            numMatrix[i][j] = fraction(document.getElementById(`${i}-${j}`).value)
+        }
+    }
+
+    if(detCalc(numMatrix)[0] === 0){
+        document.getElementById('inverse').innerHTML=document.getElementById('inverse').innerHTML+ "없습니다!"
+    }
+    
+    else{
+    const inverse = gauss(numMatrix);
+
+    for(let i=0;i<val;i++){
+        jaxMatrix[i] = ""
+        for(let j=0;j<val;j++){
+            if(j !== val-1){
+                if(inverse[i][j][0] === 0){
+                    jaxMatrix[i] += "0" + " & ";
+                }
+                else if(inverse[i][j][1] === 1){
+                    jaxMatrix[i] += `${inverse[i][j][0]}` + " & ";
+                }
+                else{
+                    jaxMatrix[i] += "\\frac{"+`${inverse[i][j][0]}`+"}{"+`${inverse[i][j][1]}`+"}"+ " & "
+                }
+            }
+            else if (j === val-1 && i !== val-1){
+                if(inverse[i][j][0] === 0){
+                    jaxMatrix[i] += "0" + slash.replace("{","").replace("}","");
+                }
+                else if(inverse[i][j][1] === 1){
+                    jaxMatrix[i] += `${inverse[i][j][0]}` + slash.replace("{","").replace("}","");
+                }
+                else{
+                jaxMatrix[i] += "\\frac{"+`${inverse[i][j][0]}`+"}{"+`${inverse[i][j][1]}`+"}" + slash.replace("{","").replace("}","");
+                }
+            }
+            else{
+                if(inverse[i][j][0] === 0){
+                    jaxMatrix[i] += "0";
+                }
+                else if(inverse[i][j][1] === 1){
+                    jaxMatrix[i] += `${inverse[i][j][0]}`;
+                }
+                else{
+                jaxMatrix[i] += "\\frac{"+`${inverse[i][j][0]}`+"}{"+`${inverse[i][j][1]}`+"}"
+                }
+            }
+        }
+        jaxString += jaxMatrix[i];
+    }
+    let jaxJax = jaxString + "\\end{pmatrix}$";
+    document.getElementById('inverse').innerHTML = jaxJax;
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub,"inverse"]);
+}
+}
+
+// const transpose = (arr) => {
+//     let len = arr[0].length
+//     let flag = 0;
+//     for(let i=1;i<len;i++){
+//         for(let j=0;j<i;j++){
+//             flag = arr[i][j];
+//             arr[i][j] = arr[j][i];
+//             arr[j][i] = flag;
+//         }
+//     }
+//     return arr
+// }
+
+const gauss = (arra) => {
+    let len = arra[0].length;
+    let ideal = create2DArray(len,len);
+    let pseudo = Array(len).fill([0,1]);
+    let ipseudo = Array(len).fill([0,1]);
+    let conti;
+    let conta;
+    let conti1;
+    let conta1;
+
+    for(let i=0;i<len;i++){
+        ideal[i][i] = [1,1];
+    }
+    
+    for(let i=0;i<len;i++){
+        if(parseInt(arra[i][i][0])!==0){
+            conti1 = ideal[i].map(m=> fracDiv(m,arra[i][i]));
+            conta1 = arra[i].map(m=> fracDiv(m,arra[i][i]));
+            // console.log(conti1)
+            // console.log(conta1)
+            for(let k=0;k<len;k++){
+                ideal[i][k] = conti1[k]
+                arra[i][k] = conta1[k] 
+            }
+        }
+        else{
+            for(let j=0;j<len;j++){
+                pseudo[j] = arra[i][j];
+                ipseudo[j] = ideal[i][j];
+            }
+            for(let k=i;k<len;k++){
+                if(parseInt(arra[k][i][0])!==0){
+                    for(let p=0;p<len;p++){
+                        arra[i][p] = arra[k][p];
+                        arra[k][p] = pseudo[p];
+                        ideal[i][p] = ideal[k][p];
+                        ideal[k][p] = ipseudo[p];
+                    }
+                    conti1 = ideal[i].map(m=>fracDiv(m,arra[i][i]));
+                    conta1 = arra[i].map(m=> fracDiv(m,arra[i][i]));
+                    for(let k=0;k<len;k++){
+                        ideal[i][k] = conti1[k]
+                        arra[i][k] = conta1[k] 
+                    }
+                    break
+                }
+            }
+        }
+        for(let p=0;p<len;p++){
+            if(p !== i && parseInt(arra[p][i][0]) !== 0){
+                conti = ideal[p].map((m,index) => {return fracSub(m, fracMul(arra[p][i],ideal[i][index]))})
+                conta = arra[p].map((m,index) => {return fracSub(m,fracMul(arra[p][i],arra[i][index]))})
+                // console.log(conti)
+                // console.log(conta)
+                for(let k=0;k<len;k++){
+                    ideal[p][k] = conti[k];
+                    arra[p][k] = conta[k];
+                }
+            }
+            let g1 = gcd(ideal[p][i][0],ideal[p][i][1]);
+            let a1 = parseInt(ideal[p][i][0])/g1;
+            let b1 = parseInt(ideal[p][i][1])/g1;
+                ideal[p][i] = [a1,b1];
+        }
+
+    }
+
+    for(let i=0;i<len;i++){
+        for(let j=0;j<len;j++){
+            if(ideal[i][j][0] !== 0){
+                if(ideal[i][j][1] < 0){
+                    ideal[i][j]= fracMul(ideal[i][j],[-1,-1]);
+                }
+                let g2 = gcd(ideal[i][j][0],ideal[i][j][1]);
+                let a2 = parseInt(ideal[i][j][0])/g2;
+                let b2 = parseInt(ideal[i][j][1])/g2;
+                ideal[i][j] = [a2, b2];
+            }
+        }
+    }
+    
+    return ideal
+}
+
+
+
+
+
 // const inverseCalc = (arr) => {
 //     const len = arr[0].length
 //     const matrix = create2DArray(len,len)
@@ -244,163 +432,3 @@ const detCalc = (arr) => {
 //         return matrix
 //     }
 // }
-
-const inverse = () => {
-    let val = document.getElementById("inputNumber").value;
-    val = parseInt(val);
-    let jaxString = document.getElementById('inverse').innerHTML+"$\\begin{pmatrix}";
-    let jaxMatrix = new Array();
-    const slash = "{ \\\\ }";
-
-    let numMatrix = create2DArray(val,val);
-    for(let i=0;i<val;i++){
-        for(let j=0;j<val;j++){
-            numMatrix[i][j] = fraction(document.getElementById(`${i}-${j}`).value)
-        }
-    }
-
-    if(detCalc(numMatrix)[0] === 0){
-        document.getElementById('inverse').innerHTML=document.getElementById('inverse').innerHTML+ "없습니다!"
-    }
-    
-    else{
-    const inverse = gauss(numMatrix);
-
-    for(let i=0;i<val;i++){
-        jaxMatrix[i] = ""
-        for(let j=0;j<val;j++){
-            if(j !== val-1){
-                if(inverse[i][j][0] === 0){
-                    jaxMatrix[i] += "0" + " & ";
-                }
-                else if(inverse[i][j][1] === 1){
-                    jaxMatrix[i] += `${inverse[i][j][0]}` + " & ";
-                }
-                else{
-                    jaxMatrix[i] += "\\frac{"+`${inverse[i][j][0]}`+"}{"+`${inverse[i][j][1]}`+"}"+ " & "
-                }
-            }
-            else if (j === val-1 && i !== val-1){
-                if(inverse[i][j][0] === 0){
-                    jaxMatrix[i] += "0" + slash.replace("{","").replace("}","");
-                }
-                else if(inverse[i][j][1] === 1){
-                    jaxMatrix[i] += `${inverse[i][j][0]}` + slash.replace("{","").replace("}","");
-                }
-                else{
-                jaxMatrix[i] += "\\frac{"+`${inverse[i][j][0]}`+"}{"+`${inverse[i][j][1]}`+"}" + slash.replace("{","").replace("}","");
-                }
-            }
-            else{
-                if(inverse[i][j][0] === 0){
-                    jaxMatrix[i] += "0";
-                }
-                else if(inverse[i][j][1] === 1){
-                    jaxMatrix[i] += `${inverse[i][j][0]}`;
-                }
-                else{
-                jaxMatrix[i] += "\\frac{"+`${inverse[i][j][0]}`+"}{"+`${inverse[i][j][1]}`+"}"
-                }
-            }
-        }
-        jaxString += jaxMatrix[i];
-    }
-    let jaxJax = jaxString + "\\end{pmatrix}$";
-    document.getElementById('inverse').innerHTML = jaxJax;
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub,"inverse"]);
-}
-}
-
-// const transpose = (arr) => {
-//     let len = arr[0].length
-//     let flag = 0;
-//     for(let i=1;i<len;i++){
-//         for(let j=0;j<i;j++){
-//             flag = arr[i][j];
-//             arr[i][j] = arr[j][i];
-//             arr[j][i] = flag;
-//         }
-//     }
-//     return arr
-// }
-
-const gauss = (arra) => {
-    let len = arra[0].length;
-    let ideal = create2DArray(len,len);
-    let pseudo = Array(len).fill([0,1]);
-    let ipseudo = Array(len).fill([0,1]);
-    let conti;
-    let conta;
-
-    for(let i=0;i<len;i++){
-        ideal[i][i] = [1,1];
-    }
-    
-    for(let i=0;i<len;i++){
-        if(parseInt(arra[i][i][0])!==0){
-            conti = ideal[i].map(m=>[parseInt(arra[i][i][1])*parseInt(m[0]),parseInt(arra[i][i][0])*parseInt(m[1])]);
-            conta = arra[i].map(m=> [parseInt(arra[i][i][1])*parseInt(m[0]),parseInt(arra[i][i][0])*parseInt(m[1])]);
-            for(let k=0;k<len;k++){
-                ideal[i][k] = conti[k]
-                arra[i][k] = conta[k] 
-            }
-        }
-        else{
-            for(let j=0;j<len;j++){
-                pseudo[j] = arra[i][j];
-                ipseudo[j] = ideal[i][j];
-            }
-            for(let k=i;k<len;k++){
-                if(parseInt(arra[k][i][0])!==0){
-                    for(let p=0;p<len;p++){
-                        arra[i][p] = arra[k][p];
-                        arra[k][p] = pseudo[p];
-                        ideal[i][p] = ideal[k][p];
-                        ideal[k][p] = ipseudo[p];
-                    }
-                    conti = ideal[i].map(m=>[parseInt(arra[i][i][1])*parseInt(m[0]),parseInt(arra[i][i][0])*parseInt(m[1])]);
-                    conta = arra[i].map(m=> [parseInt(arra[i][i][1])*parseInt(m[0]),parseInt(arra[i][i][0])*parseInt(m[1])]);
-                    for(let k=0;k<len;k++){
-                        ideal[i][k] = conti[k]
-                        arra[i][k] = conta[k] 
-                    }
-                    break
-                }
-            }
-        }
-        for(let p=0;p<len;p++){
-            if(p !== i && parseInt(arra[p][i][0]) !== 0){
-                conti = ideal[p].map((m,index) => {return fracSum([parseInt(m[0]),parseInt(m[1])],[parseInt(arra[p][i][0])*parseInt(ideal[i][index][0])*Math.pow(-1,1), parseInt(arra[p][i][1])*parseInt(ideal[i][index][1])])})
-                conta = arra[p].map((m,index) => {return fracSum([parseInt(m[0]),parseInt(m[1])],[parseInt(arra[p][i][0])*parseInt(arra[i][index][0])*Math.pow(1,-1), parseInt(arra[p][i][1])*parseInt(arra[i][index][1])])})
-                for(let k=0;k<len;k++){
-                    ideal[p][k] = conti[k];
-                    arra[p][k] = conta[k];
-                }
-            }
-            let g = parseInt(gcd(parseInt(ideal[i][p][0]),parseInt(ideal[i][p][1])));
-                ideal[i][p] = [parseInt(ideal[i][p][0])/g, parseInt(ideal[i][p][1])/g];
-        }
-
-    }
-
-    for(let i=0;i<len;i++){
-        for(let j=0;j<len;j++){
-            if(ideal[i][j][0] !== 0){
-                if(ideal[i][j][1] < 0){
-                    ideal[i][j]= [parseInt(ideal[i][j][0])*Math.pow(-1,1), parseInt(ideal[i][j][1])*Math.pow(-1,1)]
-                }
-                let g = parseInt(gcd(parseInt(ideal[i][j][0]),parseInt(ideal[i][j][1])));
-                ideal[i][j] = [parseInt(ideal[i][j][0])/g, parseInt(ideal[i][j][1])/g];
-            }
-        }
-    }
-    console.log(ideal)
-    console.log(arra)
-    return ideal
-}
-
-// console.log(gauss([[[134,1],[1324,1],[8,1],[239,1],[378,1]],[[189,1],[209,1],[3,1],[278,1],[892,1]],[[1920,1],[4,1],[0,1],[189,1],[19,1]],[[389,1],[203,1],[390,1],[183,1],[10,1]],[[9,1],[20,1],[4,1],[0,1],[1,1]]]))
-
-// console.log(gauss([[[192,1],[283,1],[102,1],[494,1]],[[281,1],[364,1],[203,1],[901,1]],[[29,1],[789,1],[182,1],[4567,1]],[[32,1],[293,1],[384,1],[456,1]]]))
-
-gauss([[[192,1],[283,1],[102,1]],[[281,1],[364,1],[203,1]],[[29,1],[789,1],[182,1]]])
