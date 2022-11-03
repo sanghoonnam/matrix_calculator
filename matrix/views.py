a54460a2989e55d82a836bf62dadd18f46237de6
sanghoonnam <yoodotan@snu.ac.kr>
2022-11-03 13:25:54 +0900

    add deter

--- matrix/views.py
+++ matrix/views.py
@@ -9,49 +9,58 @@ def index(request):
 
 def inverseEn(request):
     return render(request, "matrix/inverseEn.html")   
-
 def inverseKo(request):
     return render(request, "matrix/inverseKo.html")
-
 def inverseJa(request):
     return render(request, "matrix/inverseJa.html")
+def inverseCh(request):
+    return render(request, "matrix/inverseCh.html")
+def inverseSp(request):
+    return render(request, "matrix/inverseSp.html")
+def inverseVi(request):
+    return render(request, "matrix/inverseVi.html") 
+def inversePo(request):
+    return render(request, "matrix/inversePo.html")
+def inverseIn(request):
+    return render(request, "matrix/inverseIn.html")
+def inverseRu(request):
+    return render(request, "matrix/inverseRu.html")
 
 def indexEn(request):
     return render(request, "matrix/indexEn.html")
-
 def indexKo(request):
     return render(request, "matrix/indexKo.html")
-
 def indexJa(request):
     return render(request, "matrix/indexJa.html")    
-
 def indexCh(request):
-    return render(request, "matrix/indexCh.html")
-
-def inverseCh(request):
-    return render(request, "matrix/inverseCh.html") 
-
+    return render(request, "matrix/indexCh.html") 
 def indexSp(request):
     return render(request, "matrix/indexSp.html") 
-def inverseSp(request):
-    return render(request, "matrix/inverseSp.html") 
-
 def indexVi(request):
     return render(request, "matrix/indexVi.html") 
-def inverseVi(request):
-    return render(request, "matrix/inverseVi.html") 
-
 def indexPo(request):
     return render(request, "matrix/indexPo.html") 
-def inversePo(request):
-    return render(request, "matrix/inversePo.html")
-
 def indexIn(request):
     return render(request, "matrix/indexIn.html") 
-def inverseIn(request):
-    return render(request, "matrix/inverseIn.html")
-
 def indexRu(request):
     return render(request, "matrix/indexRu.html") 
-def inverseRu(request):
-    return render(request, "matrix/inverseRu.html")
+
+
+def deterEn(request):
+    return render(request, "matrix/deterEn.html")
+def deterKo(request):
+    return render(request, "matrix/deterKo.html")
+def deterJa(request):
+    return render(request, "matrix/deterJa.html")    
+def deterCh(request):
+    return render(request, "matrix/deterCh.html") 
+def deterSp(request):
+    return render(request, "matrix/deterSp.html") 
+def deterVi(request):
+    return render(request, "matrix/deterVi.html") 
+def deterPo(request):
+    return render(request, "matrix/deterPo.html") 
+def deterIn(request):
+    return render(request, "matrix/deterIn.html") 
+def deterRu(request):
+    return render(request, "matrix/deterRu.html")
