ecec27f8082d90a2372632f8a6e7784bcccf4213
유상훈 <yusanghoon@yusanghun-ui-MacBookAir.local>
2023-07-19 10:34:05 +0900

    add prime det kor version

--- matrix/static/matrix/js/prime.js
+++ matrix/static/matrix/js/prime.js
@@ -1,20 +1,24 @@
 const primeDet = () => {
     let prime = document.getElementById("inputPrime").value;
     document.getElementById("primeResult").innerHTML = document.getElementById("primeResult").innerHTML.split(":")[0]+": ";
+    box = prime;
+    const regExp = /^[0-9]+$/;
     prime = parseInt(prime);
     
     root = parseInt(Math.sqrt(prime));
     
     // var arr = new Array(root);
-    
-    if(root==1 && prime!=1){
+    if(prime<=0){
+        document.getElementById("primeResult").innerHTML=document.getElementById("primeResult").innerHTML+"소수가 아닙니다."
+    }
+    else if(root==1 && prime!=1){
         document.getElementById("primeResult").innerHTML=document.getElementById("primeResult").innerHTML+"소수입니다."
     }
     else if(prime==1){
         document.getElementById("primeResult").innerHTML=document.getElementById("primeResult").innerHTML+"소수가 아닙니다."
     }
     
-    else{   
+    else if(regExp.test(box)){   
     let sum=0;
     for(let i=1;i<root;i++){
     if(prime % (i+1)==0){
@@ -33,4 +37,9 @@ const primeDet = () => {
         document.getElementById("primeResult").innerHTML=document.getElementById("primeResult").innerHTML+"소수입니다."
     }
     }
+    else{
+        document.getElementById("primeResult").innerHTML=document.getElementById("primeResult").innerHTML+"숫자가 아닙니다."
+    }
 }
+
+// 변수 예외처리 필요
